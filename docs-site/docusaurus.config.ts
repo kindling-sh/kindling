40747fdbc8c1a7a98fc1efa350ca49dbb140c3e0
jeff.vincent <jeff.d.vincent@gmail.com>
2026-02-22 01:56:35 -0800

    docs: default to dark mode, raise hero content on landing page

--- docs-site/docusaurus.config.ts
+++ docs-site/docusaurus.config.ts
@@ -49,7 +49,7 @@ const config: Config = {
     colorMode: {
       defaultMode: "dark",
       disableSwitch: false,
-      respectPrefersColorScheme: true,
+      respectPrefersColorScheme: false,
     },
     navbar: {
       title: "kindling",
